19644af35db8fcc6936848a0a004322eadcc7ae5
ItsTiers-Tierlist <itsmoritzzz3012@gmail.com>
2026-07-01 22:12:07 +0200

    script.js

--- script.js
+++ script.js
@@ -87,8 +87,9 @@ const players = {
     name: "McNicho",
     rank: "3.",
     title: "Combat Cadet",
-    points: "35 points",
+    points: "74 points",
     tiers: [
+      {icon:"vanilla", tier:"LT1"},
       {icon:"mace", tier:"HT3"},      
       {icon:"https://subtiers.net/assets/trident-1c1a3e5a.svg", tier:"LT4"},
       {icon:"sword", tier:"LT4"},
@@ -98,7 +99,7 @@ const players = {
       {icon:"nethop", tier:"HT5"},
       {icon:"uhc", tier:"HT5"},      
       {icon:"axe", tier:"HT5"},
-      {icon:"vanilla", tier:"RLT3"},
+      
       
     ]
   },
