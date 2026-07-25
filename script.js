c34333d5903e6b1dcf076036cb60cbce295dfbe5
ItsTiers-Tierlist <itsmoritzzz3012@gmail.com>
2026-07-25 12:50:31 +0200

    script.js

--- script.js
+++ script.js
@@ -122,11 +122,11 @@ const players = {
       {icon:"pot", tier:"LT3"},
       {icon:"nethop", tier:"HT4"},
       {icon:"mace", tier:"LT4"},
-      {icon:"https://subtiers.net/assets/trident-1c1a3e5a.svg", tier:"LT4"},      
-      {icon:"https://cdn-icons-png.magnific.com/512/6428/6428889.png", tier:"HT5"},
+      {icon:"https://subtiers.net/assets/trident-1c1a3e5a.svg", tier:"LT4"},            
       {icon:"vanilla", tier:"HT5"},
       {icon:"smp", tier:"HT5"}, 
       {icon:"axe", tier:"HT5"},
+      {icon:"https://cdn-icons-png.magnific.com/512/6428/6428889.png", tier:"LT5"},
       {icon:"uhc", tier:"LT5"},
                   
                                                                
