7c6ecb6da2b970ebb23ff2c51bb8eb26b113ce13
ItsTiers-Tierlist <itsmoritzzz3012@gmail.com>
2026-07-28 00:13:15 +0200

    script.js

--- script.js
+++ script.js
@@ -101,6 +101,7 @@ const players = {
     points: "36 points",
     tiers: [     
       {icon:"mace", tier:"HT3"},  
+      {icon:"vanilla", tier:"LT3"}, 
       {icon:"sword", tier:"HT4"},
       {icon:"https://subtiers.net/assets/trident-1c1a3e5a.svg", tier:"LT4"},     
       {icon:"https://cdn-icons-png.magnific.com/512/6428/6428889.png", tier:"LT4"},
@@ -109,7 +110,7 @@ const players = {
       {icon:"nethop", tier:"HT5"},
       {icon:"uhc", tier:"HT5"},      
       {icon:"axe", tier:"HT5"},
-      {icon:"vanilla", tier:"RLT3"},      
+           
     ]
   },
 Eliiiiaass: {
