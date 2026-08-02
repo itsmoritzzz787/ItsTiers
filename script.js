baaf8a2bf68de1b34b700f10cac1e9bdcd4f6d4a
ItsTiers-Tierlist <itsmoritzzz3012@gmail.com>
2026-08-02 02:09:09 +0200

    script.js

--- script.js
+++ script.js
@@ -188,9 +188,28 @@ const players = {
       {icon:"nethop", tier:"LT5"},
     ]
   }, 
+ shotss: {
+    name: "4shotss",
+    rank: "9.",
+    title: "Combat Cadet",
+    points: "26 points",
+    tiers: [
+      {icon:"https://subtiers.net/assets/trident-1c1a3e5a.svg", tier:"LT3"},
+      {icon:"pot", tier:"LT3"},
+      {icon:"sword", tier:"LT3"},
+      {icon:"nethop", tier:"HT4"},
+      {icon:"mace", tier:"HT4"}, 
+      {icon:"smp", tier:"LT4"},
+      {icon:"axe", tier:"LT5"},
+      {icon:"vanilla", tier:"-"},                          
+      {icon:"uhc", tier:"-"},                  
+      {icon:"https://cdn-icons-png.magnific.com/512/6428/6428889.png", tier:"-"},
+      
+    ]
+  },
  SocialRex11: {
     name: "SocialRex11",
-    rank: "9.",
+    rank: "10.",
     title: "Combat Cadet",
     points: "28 points",
     tiers: [   
@@ -206,25 +225,7 @@ const players = {
       {icon:"smp", tier:"HT5"},                             
     ]
   }, 
- shotss: {
-    name: "4shotss",
-    rank: "10.",
-    title: "Combat Cadet",
-    points: "26 points",
-    tiers: [
-      {icon:"https://subtiers.net/assets/trident-1c1a3e5a.svg", tier:"LT3"},
-      {icon:"pot", tier:"LT3"},
-      {icon:"sword", tier:"LT3"},
-      {icon:"mace", tier:"HT4"}, 
-      {icon:"smp", tier:"LT4"},
-      {icon:"axe", tier:"LT5"},
-      {icon:"vanilla", tier:"-"},                
-      {icon:"nethop", tier:"-"},    
-      {icon:"uhc", tier:"-"},                  
-      {icon:"https://cdn-icons-png.magnific.com/512/6428/6428889.png", tier:"-"},
-      
-    ]
-  },
+ 
  ySw1ft: {
     name: "ySw1ft",
     rank: "11.",
