201213fa6d342cf9fc9d567937431198ff1db718
ItsTiers-Tierlist <itsmoritzzz3012@gmail.com>
2026-08-31 03:35:30 +0200

    script.js

--- script.js
+++ script.js
@@ -244,10 +244,27 @@ const players = {
       {icon:"https://subtiers.net/assets/trident-1c1a3e5a.svg", tier:"-"}
     ]
   },
-  
+   Ginoski: {
+    name: "Ginoski",
+    rank: "12.",
+    title: "Combat Cadet",
+    points: "36 points",
+    tiers: [  
+      {icon:"vanilla", tier:"LT3"},
+      {icon:"https://cdn-icons-png.magnific.com/512/6428/6428889.png", tier:"LT3"},
+      {icon:"https://subtiers.net/assets/trident-1c1a3e5a.svg", tier:"LT3"},
+      {icon:"mace", tier:"LT3"}, 
+      {icon:"sword", tier:"LT4"}, 
+      {icon:"uhc", tier:"LT4"},
+      {icon:"axe", tier:"HT5"},
+      {icon:"pot", tier:"HT5"},                
+      {icon:"smp", tier:"LT5"},
+      {icon:"nethop", tier:"LT5"},
+    ]
+  }, 
   TwyCole: {
     name: "TwyCole",
-    rank: "12.",
+    rank: "13.",
     title: "Combat Cadet",
     points: "34 points",
     tiers: [      
@@ -265,7 +282,7 @@ const players = {
   },
  Jonkler75: {
     name: "Jonkler75",
-    rank: "12.",
+    rank: "13.",
     title: "Combat Cadet",
     points: "34 points",
     tiers: [
@@ -283,7 +300,7 @@ const players = {
   }, 
  SocialRex11: {
     name: "SocialRex11",
-    rank: "12.",
+    rank: "13.",
     title: "Combat Cadet",
     points: "34 points",
     tiers: [  
@@ -301,7 +318,7 @@ const players = {
   }, 
  Gold3n__: {
     name: "Gold3n__",
-    rank: "12.",
+    rank: "13.",
     title: "Combat Cadet",
     points: "34 points",
     tiers: [   
@@ -318,24 +335,7 @@ const players = {
     ]
   },
 
- Ginoski: {
-    name: "Ginoski",
-    rank: "16.",
-    title: "Combat Cadet",
-    points: "32 points",
-    tiers: [  
-      {icon:"https://cdn-icons-png.magnific.com/512/6428/6428889.png", tier:"LT3"},
-      {icon:"https://subtiers.net/assets/trident-1c1a3e5a.svg", tier:"LT3"},
-      {icon:"mace", tier:"LT3"}, 
-      {icon:"sword", tier:"LT4"}, 
-      {icon:"uhc", tier:"LT4"},
-      {icon:"vanilla", tier:"HT5"},
-      {icon:"axe", tier:"HT5"},
-      {icon:"pot", tier:"HT5"},                
-      {icon:"smp", tier:"LT5"},
-      {icon:"nethop", tier:"LT5"},
-    ]
-  }, 
+
   lozk00: {
     name: "lozk00",
     rank: "17.",
